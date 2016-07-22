c36435bb7b8b7f60098fb20ce8405c93d298b2e9
mauroservienti <mauro@topics.it>
2016-07-22 08:14:59 +0200

    adds ShippingService

---
+++ Shipping.Data/Context/SalesContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shipping.Data.Migrations;
+using Shipping.Data.Models;
+
+namespace Shipping.Data.Context
+{
+    public interface IShippingContext
+    {
+        IQueryable<ShippingDetails> ShippingDetails { get; }
+    }
+
+    [DbConfigurationType(typeof(SqLiteConfig))]
+    public class ShippingContext : DbContext, IShippingContext
+    {
+        public ShippingContext() : base("Shipping")
+        {
+        }
+
+        public IDbSet<ShippingDetails> ShippingDetails { get; set; }
+
+        IQueryable<ShippingDetails> IShippingContext.ShippingDetails
+        {
+            get { return this.ShippingDetails; }
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Database.SetInitializer(new DatabaseInitializer(modelBuilder));
+
+            modelBuilder.Entity<ShippingDetails>()
+                .HasKey(sp => sp.Id);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Shipping.Data/Models/ShippingDetails.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shipping.Data.Models
+{
+    public class ShippingDetails
+    {
+        public int Id { get; set; }
+        public int StockItemId { get; set; }
+        public double Cost { get; set; }
+        public decimal Weight { get; set; }
+    }
+}

d4025668eeceac2448a7e2fb561468af307ae9e1
mauroservienti <mauro@topics.it>
2016-11-07 15:55:18 +0100

    adds Publising support

---
+++ Marketing.API/Controllers/PublishingController.cs
@@ -0,0 +1,27 @@
+﻿using Marketing.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Marketing.API.Controllers
+{
+    [RoutePrefix("api/Publishing")]
+    public class PublishingController : ApiController
+    {
+        private readonly IMarketingContext _context;
+
+        public PublishingController(IMarketingContext marketingRepository)
+        {
+            _context = marketingRepository;
+        }
+
+        [HttpGet]
+        public dynamic GetHomePageShowcase()
+        {
+            return null;
+        }
+    }
+}

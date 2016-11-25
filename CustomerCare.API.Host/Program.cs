7651fbf17073b2dd0a047ad7243f7ec1fefd6665
Mauro Servienti <mauro@topics.it>
2016-11-25 11:49:39 +0100

    inherits from ServiceBase

--- CustomerCare.API.Host/Program.cs
+++ CustomerCare.API.Host/Program.cs
@@ -2,23 +2,58 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CustomerCare.API.Host
 {
-    class Program
+    class Program : ServiceBase
     {
+        IDisposable webApp;
+
         static void Main(string[] args)
         {
-            Console.Title = "CustomerCare.API.Host";
+            using (var service = new Program())
+            {
+                service.ServiceName = "CustomerCare.API.Host";
+                // to run interactive from a console or as a windows service
+                if (Environment.UserInteractive)
+                {
+                    Console.Title = service.ServiceName;
+                    Console.CancelKeyPress += (sender, e) =>
+                    {
+                        service.OnStop();
+                    };
+
+                    service.OnStart(args);
+
+                    Console.WriteLine("\r\nPress enter key to stop program\r\n");
+                    Console.Read();
+
+                    service.OnStop();
+
+                    return;
+                }
+                Run(service);
+            }
+        }
+
+        protected override void OnStart(string[] args)
+        {
             string baseAddress = "http://localhost:20189/";
 
-            using(WebApp.Start<Startup>(url: baseAddress))
+            webApp = WebApp.Start<Startup>(url: baseAddress);
+
+            Console.WriteLine();
+            Console.WriteLine($"{Console.Title}: {baseAddress}");
+        }
+
+        protected override void OnStop()
+        {
+            if (webApp != null)
             {
-                Console.WriteLine();
-                Console.WriteLine($"{Console.Title}: {baseAddress}");
-                Console.ReadLine();
+                webApp.Dispose();
             }
         }
     }

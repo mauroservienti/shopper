d60199add99cef1b195c74c2dd722d5a57d8ed7b
Mauro Servienti <mauro@topics.it>
2016-11-27 11:56:59 +0100

    refactor to ServiceHost

--- CustomerCare.API.Host/Program.cs
+++ CustomerCare.API.Host/Program.cs
@@ -1,102 +1,13 @@
-﻿using Microsoft.Owin.Hosting;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Configuration.Install;
-using System.Linq;
-using System.Reflection;
-using System.ServiceProcess;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CustomerCare.API.Host
+﻿namespace CustomerCare.API.Host
 {
-    class Program : ServiceBase
+    class Program
     {
-        IDisposable webApp;
-        static string baseAddress = "http://localhost:20189/";
-
         static void Main(string[] args)
         {
-            using (var service = new Program())
+            using (var service = new ServiceHost())
             {
-                service.ServiceName = ServiceConfiguration.DisplayName;
-                // to run interactive from a console or as a windows service
-                if (Environment.UserInteractive)
-                {
-                    if (args.Contains("/install"))
-                    {
-                        ManagedInstallerClass.InstallHelper(new string[] { Assembly.GetExecutingAssembly().Location });
-                    }
-                    else if (args.Contains("/uninstall"))
-                    {
-                        ManagedInstallerClass.InstallHelper(new string[] { "/u", Assembly.GetExecutingAssembly().Location });
-                    }
-                    else
-                    {
-                        Console.Title = service.ServiceName;
-                        Console.CancelKeyPress += (sender, e) =>
-                        {
-                            service.OnStop();
-                        };
-
-                        service.OnStart(args);
-
-                        Console.WriteLine();
-                        Console.WriteLine($"{service.ServiceName}: {baseAddress}");
-
-                        Console.WriteLine("\r\nPress enter key to stop program\r\n");
-                        Console.Read();
-
-                        service.OnStop();
-                    }
-                }
-                else
-                {
-                    Run(service);
-                }
+                service.Run(args);
             }
         }
-
-        protected override void OnStart(string[] args)
-        {
-            webApp = WebApp.Start<Startup>(url: baseAddress);
-        }
-
-        protected override void OnStop()
-        {
-            if (webApp != null)
-            {
-                webApp.Dispose();
-            }
-        }
-    }
-
-    [RunInstaller(true)]
-    public class ManagedInstallation : ServiceInstaller
-    {
-        public ManagedInstallation()
-        {
-            var ProcessInstaller = new ServiceProcessInstaller();
-            var ServiceInstaller = new ServiceInstaller();
-
-            ProcessInstaller.Account = ServiceConfiguration.AccountType;
-            ServiceInstaller.DisplayName = ServiceConfiguration.DisplayName;
-            ServiceInstaller.StartType = ServiceConfiguration.StartType;
-            ServiceInstaller.Description = ServiceConfiguration.Description;
-            ServiceInstaller.ServiceName = ServiceConfiguration.ServiceName;
-
-            Installers.Add(ProcessInstaller);
-            Installers.Add(ServiceInstaller);
-        }
-    }
-
-    class ServiceConfiguration
-    {
-        public static string DisplayName => "CustomerCare API Host";
-        public static string ServiceName => "CustomerCareAPIHost";
-        public static string Description => "Hosts Custopmer Care service WebAPI.";
-        public static ServiceStartMode StartType => ServiceStartMode.Automatic;
-        public static ServiceAccount AccountType => ServiceAccount.LocalSystem;
     }
 }
---
+++ CustomerCare.API.Host/ServiceHost.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Reflection;
+using System.ServiceProcess;
+
+namespace CustomerCare.API.Host
+{
+    class ServiceHost : ServiceBase
+    {
+        IDisposable webApp;
+        string baseAddress = "http://localhost:20189/";
+        
+        protected override void OnStart(string[] args)
+        {
+            webApp = WebApp.Start<Startup>(url: baseAddress);
+        }
+
+        protected override void OnStop()
+        {
+            if (webApp != null)
+            {
+                webApp.Dispose();
+            }
+        }
+
+        internal void Run(string[] args)
+        {
+            this.ServiceName = ServiceConfiguration.ServiceName;
+            if (Environment.UserInteractive)
+            {
+                if (args.Contains("/install"))
+                {
+                    ManagedInstallerClass.InstallHelper(new string[] { Assembly.GetExecutingAssembly().Location });
+                }
+                else if (args.Contains("/uninstall"))
+                {
+                    ManagedInstallerClass.InstallHelper(new string[] { "/u", Assembly.GetExecutingAssembly().Location });
+                }
+                else
+                {
+                    Console.Title = ServiceConfiguration.DisplayName;
+                    Console.CancelKeyPress += (sender, e) =>
+                    {
+                        this.OnStop();
+                    };
+
+                    this.OnStart(args);
+
+                    Console.WriteLine();
+                    Console.WriteLine($"{this.ServiceName}: {baseAddress}");
+
+                    Console.WriteLine("\r\nPress enter key to stop program\r\n");
+                    Console.Read();
+
+                    this.OnStop();
+                }
+            }
+            else
+            {
+                Run(this);
+            }
+        }
+    }
+
+    [RunInstaller(true)]
+    public class ManagedInstallation : ServiceInstaller
+    {
+        public ManagedInstallation()
+        {
+            var ProcessInstaller = new ServiceProcessInstaller();
+            var ServiceInstaller = new ServiceInstaller();
+
+            ProcessInstaller.Account = ServiceConfiguration.AccountType;
+            ServiceInstaller.DisplayName = ServiceConfiguration.DisplayName;
+            ServiceInstaller.StartType = ServiceConfiguration.StartType;
+            ServiceInstaller.Description = ServiceConfiguration.Description;
+            ServiceInstaller.ServiceName = ServiceConfiguration.ServiceName;
+
+            Installers.Add(ProcessInstaller);
+            Installers.Add(ServiceInstaller);
+        }
+    }
+
+    class ServiceConfiguration
+    {
+        public static string DisplayName => "CustomerCare API Host";
+        public static string ServiceName => "CustomerCareAPIHost";
+        public static string Description => "Hosts Custopmer Care service WebAPI.";
+        public static ServiceStartMode StartType => ServiceStartMode.Automatic;
+        public static ServiceAccount AccountType => ServiceAccount.LocalSystem;
+    }
+}

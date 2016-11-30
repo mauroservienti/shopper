6a9611e702fac0e4925e11b3f044ba2399eb5f09
Mauro Servienti <mauro@topics.it>
2016-11-30 07:02:08 +0100

    Empty message handler

---
+++ Marketing.API.Host/Handlers/StockItemCreatedHandler.cs
@@ -0,0 +1,15 @@
+﻿using NServiceBus;
+using System;
+using System.Threading.Tasks;
+using Warehouse.StockItems.Events;
+
+namespace Marketing.API.Host.Handlers
+{
+    public class StockItemCreatedHandler : IHandleMessages<IStockItemCreatedEvent>
+    {
+        public Task Handle(IStockItemCreatedEvent message, IMessageHandlerContext context)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}

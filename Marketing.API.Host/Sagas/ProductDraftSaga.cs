00b2c52f2c313d397f4d0be336f5d4d037449597
Mauro Servienti <mauro@topics.it>
2016-12-01 08:54:12 +0100

    Message handler converted to ProductDraft saga

---
+++ Marketing.API.Host/Sagas/ProductDraftSaga.cs
@@ -0,0 +1,30 @@
+﻿using NServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Warehouse.StockItems.Events;
+
+namespace Marketing.API.Host.Sagas
+{
+    class ProductDraftSaga 
+        : Saga<ProductDraftSaga.State>,
+        IAmStartedByMessages<IStockItemCreatedEvent>
+    {
+        public class State : ContainSagaData
+        {
+            public virtual int StockItemId { get; set; }
+        }
+
+        protected override void ConfigureHowToFindSaga(SagaPropertyMapper<State> mapper)
+        {
+            mapper.ConfigureMapping<IStockItemCreatedEvent>(e => e.Id).ToSaga(s => s.StockItemId);
+        }
+
+        public Task Handle(IStockItemCreatedEvent message, IMessageHandlerContext context)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}

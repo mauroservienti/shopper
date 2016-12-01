18815c4540f1030a0c3fc36b686e9856c9483e81
Mauro Servienti <mauro@topics.it>
2016-12-01 07:18:24 +0100

    Shipping details

---
+++ Shipping.ViewComponents/ItemShippingInfoViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shipping.ViewComponents
+{
+    [ViewComponent(Name = "Shipping.ViewComponents.ItemShippingInfo")]
+    public class ItemShippingInfoViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(dynamic product)
+        {
+            return View(product.ItemShippingInfo);
+        }
+    }
+}

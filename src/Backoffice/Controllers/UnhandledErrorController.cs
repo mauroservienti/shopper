4bc82a74eaf490f70f23b383abbd3df48cbcef83
mauroservienti <mauro@topics.it>
2017-05-08 19:18:11 +0200

    backoffice empty web app

---
+++ src/Backoffice/Controllers/UnhandledErrorController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backoffice.Controllers
+{
+    public class UnhandledErrorController : Controller
+    {
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/Backoffice/Startup.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using ITOps.ViewModelComposition;
+using ITOps.ViewModelComposition.Mvc;
+using ITOps.UIComposition.Mvc;
+
+namespace Backoffice
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddViewModelComposition();
+            services
+                .AddMvc()
+                .AddViewModelCompositionMvcSupport()
+                .AddUIComposition();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            DefaultAppConfigure(app, env, loggerFactory);
+
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+
+        void DefaultAppConfigure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/UnhandledError/Error");
+            }
+        }
+    }
+}
